cf3df3b1542475497013314697ad9e736628de45
JayLVulcan <jayl@vulcan.com>
2019-12-16 11:59:24 -0800

    Put secrets in a resource file, but hide it from git

--- app/src/main/java/mobile/pamdas/org/roam/DasClient.java
+++ app/src/main/java/mobile/pamdas/org/roam/DasClient.java
@@ -16,14 +16,15 @@ public class DasClient {
     // todo make provider configurable
     private final String mProvider = "roam-mobile";
     // todo - get this out of code. maybe put this is the keystore?
-    private final String mAuthToken = "Wt39P5yRlGw9VmwxVGRx4UOmnHfXAH";
+    private final String mAuthToken;
     private final Context mContext;
     private String mDasEndpoint;
 
     public DasClient(Context ctx) {
         mContext = ctx;
+        mAuthToken = mContext.getString(R.string.das_api_token);
+        mDasEndpoint = mContext.getString(R.string.das_api_endpoint);
         AndroidNetworking.initialize(mContext.getApplicationContext());
-        setDasEndpoint("https://dev.pamdas.org/api/v1.0");
     }
 
     public void setDasEndpoint(String endpoint) {
